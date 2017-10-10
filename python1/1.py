28f25418e25aefe16bffff2ca8033e68379e6a5b
lysgwl <lysgwl@163.com>
2017-10-10 23:59:49 +0800

    2017.10.10 23:59

--- python1/1.py
+++ python1/1.py
@@ -14,6 +14,8 @@ import ConfigParser
 def main():
 	print ("this is a test!")
 
+#Make a script both importable and executable
+#让你写的脚本模块既可以导入到别的模块中用，另外该模块自己也可执行。
 if __name__=='__main__':
 	main()
 '''
