5d09c8b23f39a90390fb3074de44ec6a78c8f4c6
lei.wang <lei.wang@haitaichina.com>
2019-05-21 18:26:20 +0800

    2019.05.21 18:26 Signed-off-by: lei.wang <lei.wang@haitaichina.com>

--- python1/numpy_test.py
+++ python1/numpy_test.py
@@ -8,26 +8,34 @@ file_path = ""
 
 def numpy_test_part1():
 	file_path = os.path.abspath(os.path.join(current_path, 'a1.bin'))
+	a = np.arange(15).reshape(3,5)	#arange(最小值,最大值,步长)(左闭右开)
+	print(a)
+	#print("array a: shape=%d, ndim=%d"%(a.shape, a.ndim))	#数组的维度	#数组的行数	
+	#print("array a: type=%s, itemsize=%d"%(a.dtype.name, a.itemsize))	#数组的类型名称 #数组中每个元素字节的大小
+	#print(type(a))
+	print("array a: size=%d, sum=%d"%(a.size, a.sum()))
+	print("array a: min=%d, max=%d"%(a.min(), a.max()))
 	
-	#tofile
-	a = np.arange(0,12)
-	a.shape = 3,4
-	a.tofile(file_path)
-	print(a.dtype)
+	b = np.ones([10, 10])
+	b = np.zeros([10, 10])
+	b = np.random.rand(10, 10)		#创建指定形状(示例为10行10列)的数组(范围在0至1之间)
+	b = np.empty((10, 10))
+	b = np.random.uniform(0, 100)	#创建指定范围内的一个数
+	b = np.random.randint(0, 100)	#创建指定范围内的一个整数
+	b = np.random.normal(1.75, 0.1, (4,5))
+	b = b[1:3, 2:4]					#截取第1至2行的第2至3列(从第0行算起)
+	print(b)
 	
-	#fromfile
-	if a.dtype == 'float':
-		dtype = np.float
-	elif a.dtype == 'int32':
-		dtype = np.int32
-	else:
-		return
-		
-	b = np.fromfile(file_path, dtype=dtype)
-	print(b)
-	b.shape = 3,4
-	print(b)
-	#print(b["arr_0"])
+	c = np.array([[80,88], [82,81], [84,75], [86,83], [75,81]])
+	#c = np.where(c<80, 0, 90)		#如果数值小于80,替换为0;如果大于80,替换为90
+	#c = np.amax(c, axis=0)			#求(轴)每一列的最大值,amin
+	#c = np.amax(c, axis=1)			#求(轴)每一行的最大值,amin
+	#c = np.mean(c, axis=0)			#求(轴)每一列的平均值
+	#c = np.mean(c, axis=1)			#求(轴)每一行的平均值
+	#c = np.std(c, axis=0)			#求(轴)每一列的方差
+	#c = np.std(c, axis=1)			#求(轴)每一行的方差
+	#c[:,0] = c[:,0]+5				#为所有成绩都加5分
+	print(c)
 
 def numpy_test_part2():
 	file_path = os.path.abspath(os.path.join(current_path, 'a2.npz'))
@@ -103,11 +111,11 @@ def numpy_test_part5():
 	print(b*b) #输出数组的平方阵，即[[1, 4, 9], [16, 25, 36]]
 	
 def main():
-	#numpy_test_part1()
+	numpy_test_part1()
 	#numpy_test_part2()
 	#numpy_test_part3()
 	#numpy_test_part4()
-	numpy_test_part5()
+	#numpy_test_part5()
 	
 if __name__=='__main__':
 	main()
