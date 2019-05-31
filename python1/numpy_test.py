32ae20eaba098f9951f68d0f165994da779e8f12
lei.wang <lei.wang@haitaichina.com>
2019-05-31 17:30:12 +0800

    2019.05.31 17:30 Signed-off-by: lei.wang <lei.wang@haitaichina.com>

--- python1/numpy_test.py
+++ python1/numpy_test.py
@@ -9,12 +9,12 @@ file_path = ""
 def numpy_test_part1():
 	file_path = os.path.abspath(os.path.join(current_path, 'a1.bin'))
 	a = np.arange(15).reshape(3,5)	#arange(最小值,最大值,步长)(左闭右开)
-	print(a)
+	print("array a:%s"%a)
 	#print("array a: shape=%d, ndim=%d"%(a.shape, a.ndim))	#数组的维度	#数组的行数	
 	#print("array a: type=%s, itemsize=%d"%(a.dtype.name, a.itemsize))	#数组的类型名称 #数组中每个元素字节的大小
 	#print(type(a))
 	print("array a: size=%d, sum=%d"%(a.size, a.sum()))
-	print("array a: min=%d, max=%d"%(a.min(), a.max()))
+	print("array a: min=%d, max=%d\n"%(a.min(), a.max()))
 	
 	b = np.ones([10, 10])
 	b = np.zeros([10, 10])
@@ -24,7 +24,7 @@ def numpy_test_part1():
 	b = np.random.randint(0, 100)	#创建指定范围内的一个整数
 	b = np.random.normal(1.75, 0.1, (4,5))
 	b = b[1:3, 2:4]					#截取第1至2行的第2至3列(从第0行算起)
-	print(b)
+	print("array b:%s\n"%b)
 	
 	c = np.array([[80,88], [82,81], [84,75], [86,83], [75,81]])
 	#c = np.where(c<80, 0, 90)		#如果数值小于80,替换为0;如果大于80,替换为90
@@ -35,7 +35,14 @@ def numpy_test_part1():
 	#c = np.std(c, axis=0)			#求(轴)每一列的方差
 	#c = np.std(c, axis=1)			#求(轴)每一行的方差
 	#c[:,0] = c[:,0]+5				#为所有成绩都加5分
-	print(c)
+	print("array c:%s\n"%c)
+	
+	d = np.array([1,2.6,3], dtype=np.float64)
+	d = d.astype(np.int32)
+	d = np.array(['1', '2', '3'], dtype=np.string_)
+	d = d.astype(np.int32)
+	d = d*2
+	print("array d:%s"%d)
 
 def numpy_test_part2():
 	file_path = os.path.abspath(os.path.join(current_path, 'a2.npz'))
@@ -109,13 +116,53 @@ def numpy_test_part5():
 	a.sort() #将a的元素从小到大排序，此操作直接修改a，因此这时候a为[0, 1, 2, 5]
 	b= np.array([[1, 2, 3], [4, 5, 6]]) #创建二维数组
 	print(b*b) #输出数组的平方阵，即[[1, 4, 9], [16, 25, 36]]
+		
+def showinit():
+	ret = 0
+	value = input("Please input index:").strip()
+	if value.isdigit() :
+		ret = int(value)
+	else :
+		ret = -1
+		
+	return ret	
+	
+def showmenu(flag):
+	ret = 0
+	if flag == True :
+		print("*****Python Numpy Test*****")
+		print("0: show test menu!")
+		print("1: numpy test1!")
+		print("2: numpy test2!")
+		print("3: numpy test3!")
+		print("4: numpy test4!")
+	
+	for index in range (3) :
+		ret = showinit()
+		if ret < 0 or ret > 4:
+			print("Please input: 1 - 4!")
+		else :
+			break;
+			
+	return ret
 	
 def main():
-	numpy_test_part1()
-	#numpy_test_part2()
-	#numpy_test_part3()
-	#numpy_test_part4()
-	#numpy_test_part5()
-	
+	flag = True;
+
+	while True:
+		ret = showmenu(flag)
+		if ret == 0 :
+			flag = True
+			os.system("cls")
+		elif ret == 1 :
+			numpy_test_part1()
+			break
+		elif ret == 2 :
+			numpy_test_part2()
+		elif ret == 3 :	
+			numpy_test_part3()
+		elif ret == 4 :	
+			numpy_test_part4()
+			
 if __name__=='__main__':
 	main()
