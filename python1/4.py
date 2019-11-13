4eee85884b74954740b0ec05b49e611f640fe28a
lei.wang <lei.wang@haitaichina.com>
2019-11-13 18:24:04 +0800

    2019.11.13 18:24 Signed-off-by: lei.wang <lei.wang@haitaichina.com>

--- python1/4.py
+++ python1/4.py
@@ -3,3 +3,43 @@
 import os
 import sys
 
+from time import ctime
+from urllib import request
+from threading import Thread, Lock
+
+class CTest(Thread) :
+	def __init__(self, func, args, name='') :
+		Thread.__init__(self)
+		self.func = func
+		self.args = args
+		self.name = name
+		
+	def run(self) :
+		print("Test1:", ctime())
+		self.res = self.func(*self.args)
+		print("Test2:", ctime())
+	
+	def getresult(self) :
+		return self.res
+		
+def thread_run1(x) :
+	print("thread_run1 = %d"%(x+1))
+	return x+1
+	
+def main() :
+	url = "http://168.130.6.18/login.php"
+	
+	threads = []
+	for i in range(3) :
+		t = CTest(thread_run1, (i,), thread_run1.__name__)
+		threads.append(t)
+		
+	for i in range(3) :
+		threads[i].start()
+		
+	for i in range(3) :
+		threads[i].join()
+		print(threads[i].getresult())
+		
+if __name__ == "__main__" :
+	main()	
--- python1/download_test.py
+++ python1/download_test.py
@@ -3,13 +3,15 @@ import os
 import re
 import sys
 
-import time
-import zipfile
-
 from io import StringIO
 from io import BytesIO
+
+from time import ctime
+from contextlib import closing
+
 from urllib import request
 from urllib import parse as urlparse
+from threading import Thread, Lock
 
 abs_path = os.path.abspath(__file__)
 current_path = os.path.dirname(abs_path)
@@ -17,63 +19,101 @@ current_path = os.path.dirname(abs_path)
 root_path = os.path.abspath(os.path.join(current_path, os.pardir))
 download_path = os.path.abspath(os.path.join(current_path, 'downloads'))
 
-def get_filename(url, openurl=False) :
-	try :
-		if openurl == False :
-			return os.path.basename(url)
+class DownloadThread(Thread) :
+	def __init__(self, func, args, name='') :
+		Thread.__init__(self)
+		self.func = func
+		self.args = args
+		self.name = name
 		
-		req = request.urlopen(url)
-		if req.info().has_key('Content-Disposition') :
-			filename = req.info()['Content-Disposition'].split('filename=')[1]
-			filename = filename.replace('"', '').replace("'", "")
-			return filename
-		elif req.url != url	:
-			return os.path.basename(urlparse.urlsplit(req.url)[2])
-		else :
-			return os.path.basename(url)
-	except :
-		return os.path.basename(url)
+	def run(self) :
+		self.res = self.func(*self.args)
 		
-def get_parseurl(url, openurl=None) :
-	result = urlparse.urlsplit(url)
-	urlpath = "%s:\\%s\%s"%(result.scheme, result.netloc, result.path)
+	def result(self) :
+		return self.res
 		
-def get_urlfilesize(url) :
-	try :
-		req = request.urlopen(url)
-		headers = req.info().headers
+class DownloadFile() :
+	def __init__(self, url, proxies=None) :	
+		self.url = url
+		self.proxies = proxies
 		
-		length = 0
-		for header in headers :
-			if header.find('Length') != -1 :
-				length = header.split(':')[-1].strip
-				length = int(length)
-		return length
-	except :
-		return -1
-	
-def get_urldata(url, dataBuf) :
-	try:
-		req = request.urlopen(url)
-		
-		CHUNK = 16*1024
-		while True:
-			chunk = req.read(CHUNK)
-			if not chunk : break
-			dataBuf.write(chunk)
-		return True
-		
-	except:
-		return False
+	def get_filename(self, openurl=False) :
+		try :
+			if openurl == False :
+				return os.path.basename(self.url)
 				
-def download_urldata(url) :
-	dataBuf = BytesIO()
-	print(get_urlfilesize(url))
+			req = request.urlopen(self.url)
+			if req.info().has_key('Content-Disposition') :
+				filename = req.info()['Content-Disposition'].split('filename=')[1]
+				filename = filename.replace('"', '').replace("'", "")
+				return filename
+			elif req.url != self.url	:
+				return os.path.basename(urlparse.urlsplit(req.url)[2])
+			else :
+				return os.path.basename(self.url)
+		except :
+			return os.path.basename(self.url)
+			
+	def get_filesize(self) :
+		try :
+			with closing (request.urlopen(self.url, self.proxies)) as req :
+				length = req.info().get('Content-Length')
+			if length is None :
+				return 0
+			else :
+				return int(length)
+		except :
+			return -1
+			
+	def get_urldata(self, dataBuf) :
+		try :
+			
+			#req = request.urlopen(self.url)
+			
+			#CHUNK = 16*1024
+			#while True:
+			#	chunk = req.read(CHUNK)
+			#	if not chunk : break
+			#	dataBuf.write(chunk)
+			print(self.url)
+			return True
+		except :
+			return False
+
+class DownloadUrl() :
+	def __init__(self, url, path, blocks=5, proxies=None) :
+		self.url = url
+		self.path = path
+		self.blocks = blocks
+		self.proxies = proxies
+			
+	def get_parseurl(self, openurl=None) :
+		result = urlparse.urlsplit(self.url)
+		urlpath = "%s:\\%s\%s"%(result.scheme, result.netloc, result.path)
+
+	def download_urldata(self) :
+		urlfile = DownloadFile(self.url, self.proxies)
+		#totalsize = urlfile.get_filesize()
+		#if totalsize <= 0 :
+		#	return
+		
+		tasks = []
+		dataBuf = BytesIO()
+		
+		for i in range(0, self.blocks) :
+			task = DownloadThread(urlfile.get_urldata, (dataBuf,), "111")
+			task.setDaemon(True)
+			task.start()
+			tasks.append(task)
 	
 def main():
 	#download_url = "http://192.168.2.172/sdkMethod/userNumberRuleClass.php"
 	download_url = "http://168.130.6.18/login.php"
-	download_urldata(download_url)
+	#download_url = "https://www.baidu.com"
+	#download_url = "https://archive.mozilla.org/pub/seamonkey/releases/2.49.5/win32/en-US/seamonkey-2.49.5.installer.exe"
+	
+	download = DownloadUrl(download_url, "d:\\")
+	download.download_urldata()
 	
 if __name__ == "__main__":
 	main()
